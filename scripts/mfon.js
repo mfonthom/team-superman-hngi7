e6793ce0c0625705e57fb6163abafe5407892f88
Mfonabasi <58877540+mfonthom@users.noreply.github.com>
2020-06-06 23:40:08 +0100

    Update mfon.js

--- scripts/mfon.js
+++ scripts/mfon.js
@@ -3,7 +3,7 @@ function sayHello(fullname, id, language, email) {
   fullname = "Mfonabasi Thompson";
   id = "HNG-04400";
   language = "Javascript";
-  email = "mfonabasithompson@gmail.com";
+  email = "thompsonmfonabasi@gmail.com";
   
   console.log(
     `Hello World, this is ${fullname} with HNGi7 ${id} using ${language} for stage 2 task.${email}`
